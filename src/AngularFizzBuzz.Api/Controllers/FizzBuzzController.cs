53db7e796f57293f80b6de43b13818d307fe6015
lwhitlocke <lwhitlocke@unilink.com>
2019-10-26 21:59:46 +0100

    Add project files.

---
+++ src/AngularFizzBuzz.Api/Controllers/FizzBuzzController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AngularFizzBuzz.Api.Models;
+
+namespace AngularFizzBuzz.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FizzBuzzController : Controller
+    {
+        // GET api/fizzbuzz/rules
+        [Route("rules")]
+        [HttpGet]
+        public IEnumerable<FizzBuzzRule> Get()
+        {
+            var fizzBuzzRules = new List<FizzBuzzRule>()
+            {
+                new FizzBuzzRule() { DisplayResult = "Fizz", Operator = "%", Value = 3, OperationResult = 0},
+                new FizzBuzzRule() { DisplayResult = "Buzz", Operator = "%", Value = 5, OperationResult = 0},
+                new FizzBuzzRule() { DisplayResult = "Boom", Operator = ">", Value = 50}
+            };
+
+            return fizzBuzzRules;
+        }
+    }
+}
---
+++ src/AngularFizzBuzz.Api/Models/FizzBuzzRule.cs
@@ -0,0 +1,10 @@
+﻿namespace AngularFizzBuzz.Api.Models
+{
+    public class FizzBuzzRule
+    {
+        public string DisplayResult { get; set; }
+        public string Operator { get; set; }
+        public int Value { get; set; }
+        public double? OperationResult { get; set; }
+    }
+}
